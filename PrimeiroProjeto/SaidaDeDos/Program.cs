0a6a178bee8152033ec6b68ffdbd181342f462af
marcotuliosr <marcotuliopessoal@gmail.com>
2022-11-29 23:02:02 -0300

    estrutura-sequencial

---
+++ PrimeiroProjeto/SaidaDeDos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais");
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            Console.ReadLine();
+        }
+    }
+}
+
